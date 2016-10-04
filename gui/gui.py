e6e4bdf5aec9850d0c375a696ab81fcf7722baa5
MarcosPieras <pierasmarcos@gmail.com>
2016-10-04 12:20:55 +0200

    Third version: Applies the recognition using LeNet and shows the results in the command line.

--- gui/gui.py
+++ gui/gui.py
@@ -7,20 +7,31 @@ import sys
 
 class Gui(QtGui.QWidget):
 
+    IMAGE_COLS_MAX=640
+    IMAGE_ROWS_MAX=360
     updGUI=QtCore.pyqtSignal()
+
     def __init__(self, parent=None):
 
         QtGui.QWidget.__init__(self, parent)
         self.setWindowTitle("CamaraReal")
-        self.imgLabel=QtGui.QLabel(self)
-        self.imgLabel.show()
+        #self.imgLabel=QtGui.QLabel(self)
+        self.resize(6000,6000)
+        #self.imgLabel.show()
         self.updGUI.connect(self.update)
 
-        TestButton=QtGui.QPushButton("Test")
-        TestButton.move(50, 50)
-        TestButton.resize(40,40)
+
+        TestButton=QtGui.QPushButton("Evaluar")
+        TestButton.move(20, 20)
+        TestButton.resize(100,50)
         TestButton.setParent(self)
         TestButton.clicked.connect(self.effect)
+
+        self.imgLabel=QtGui.QLabel(self)
+        self.imgLabel.resize(self.IMAGE_COLS_MAX,self.IMAGE_ROWS_MAX)
+        self.imgLabel.move(100,100)
+        self.imgLabel.show()
+
 
     def setControl(self,control):
         self.control=control
